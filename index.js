91e0940058b29f367f320b12ef6a219e7e2c3fc3
Brandon Haupt <BrandonLHaupt@gmail.com>
2025-01-22 08:12:36 -0600

    changed css

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ console.log(project)
 Array.from(project).forEach((e) => {
     e.addEventListener("click", ()=> {
         e.classList.toggle("active")
+        e.classList.toggle("bgChange")
         console.log(`${e} was clicked`)
     })
 })
